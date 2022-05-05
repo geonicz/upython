d26c32a1c4a8befd99490f59098991988ef7835f
geonicz <geonicz@gmail.com>
2022-05-05 20:26:30 +0300

    initial

---
+++ main.py
@@ -0,0 +1,15 @@
+# main.py -- put your code here!
+import uos, _onewire, network, urandom, \
+    _uasyncio, onewire, ubinascii, ure, \
+    builtins, pyb, ucollections, uselect, \
+    cmath, stm, uctypes, usocket, \
+    dht, sys, uerrno, ustruct, \
+    framebuf, uarray, uhashlib, utime, gc, \
+    uheapq, utimeq, \
+    lcd160cr, uio, uzlib, \
+    lcd160cr_test, ujson, \
+    math, umachine, os
+
+# print("hello")
+# help('modules')
+dir()
